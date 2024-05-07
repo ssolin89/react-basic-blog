c9e031fc675a516383535ade35e163989db76ae0
ellie <gyeongjin.hong@dailyh.org>
2024-05-07 16:01:29 +0900

    + react 기본정리 폴더

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  let post = '강남 우동 맛집';
+  
+  //JSX 문법 2
+  // 변수를 html에 꽂아넣을때 {중괄호}
+  // 데이터바인딩 형식 사용
+  
+  //JSX 문법 3
+  // style 넣을땐 style= { {스타일명 : '값'} }
+  // 오브젝트 형식으로 넣어줌
+
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="blackNav">
+        <h4  style={{color:'red'}}>블로그</h4>
+      </div>
+      
+      <h4>{post}</h4>
     </div>
   );
 }
